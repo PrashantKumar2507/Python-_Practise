902b0abddeb00c4bba1a525e3e25dc6644cacbef
Prashant Kumar Upadhyay <80049821+PrashantKumar2507@users.noreply.github.com>
2021-03-19 20:13:42 +0530

    Simple_Interest

---
+++ Simple_Interest.py
@@ -0,0 +1,9 @@
+# Simple Interest :
+
+p= eval(input("Enter the principle : "))
+r= eval(input("Enter the rate : "))
+t= eval(input('Enter the time: '))
+
+si = (p*r*t)/100
+
+print(f"The Simple Interest is : {si}")
