55e7909c74a7f35d6b7f6d9baf87812bd96dc644
Prashant Kumar Upadhyay <80049821+PrashantKumar2507@users.noreply.github.com>
2021-03-19 20:08:09 +0530

    Swapped_no.

---
+++ Swapping_Two_no..py
@@ -0,0 +1,9 @@
+# Swapping the two no.
+
+n1 = eval(input("Enter the first no. : "))
+n2 = eval(input("Enter the second no. "))
+
+tmp=n1
+n1=n2
+n2=tmp
+print(f"The swapped first no. is: {n1} and second no. is : {n2} ")
