d978fc30d4ff55b7e83e18818a352c63fa7875f0
Prashant Kumar Upadhyay <80049821+PrashantKumar2507@users.noreply.github.com>
2021-03-21 17:08:55 +0530

    Ball_Game

---
+++ Pin_ball_game.py
@@ -0,0 +1,13 @@
+
+n= int(input("Enter the no. of ball do you want : "))
+
+if n<5:
+   if n%2==0:
+      print("You loose the game")
+      print("Computer won the game") 
+   else:
+      print("Computer has won the game.")
+else:
+   print("You can't play the game ,Please try again later .")
+       
+    
